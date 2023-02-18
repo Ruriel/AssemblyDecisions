53f8a0d8444b213795e7ee9c8ec1c931b89e8921
Ruriel <ruriel.santos@gmail.com>
2023-02-18 11:16:45 -0300

    Adding Spring Boot.

---
+++ src/main/java/com/ruriel/assembly/AssemblyDecisionsApplication.java
@@ -0,0 +1,13 @@
+package com.ruriel.assembly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblyDecisionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssemblyDecisionsApplication.class, args);
+	}
+
+}
